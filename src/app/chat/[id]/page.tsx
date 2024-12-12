15f86cab2ea64297d3c806f70f90b20230a10e94
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-12-13 00:28:13 +0900

    채팅 메시지 데이터 저장 및 가져오기 구현

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,18 +1,59 @@
 "use client";
 
-import SideBar from "@/components/SideBar";
 import { useParams } from "next/navigation";
+import SideBar from "@/components/SideBar";
+import TopBar from "@/components/TopBar";
+import BottomBar from "@/components/BottomBar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "@/firebase/firebase";
+import { User } from "firebase/auth";
+import { collection, doc, orderBy, query } from "firebase/firestore";
+import {
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
+import { CgSpinner } from "react-icons/cg";
+import { IoChatbubbleOutline } from "react-icons/io5";
 
 const ChatPage = () => {
   const { id } = useParams();
+  const [user] = useAuthState(auth);
+
+  const q = query(
+    collection(db, "chats", id as string, "messages"),
+    orderBy("timestamp")
+  );
+
+  const [messages, loading] = useCollectionData(q);
+
+  const [chat] = useDocumentData(doc(db, "chats", id as string));
 
   return (
     <main className="grid w-full grid-cols-8">
       <div className="col-span-2">
         <SideBar selectedChatId={id as string} />
       </div>
-      <div className="col-span-6 flex justify-center h-screen">
-        <div className="flex flex-col items-center justify-center space-y-4"></div>
+      <div className="flex flex-col w-full col-span-6">
+        <TopBar />
+        <div className="flex w-full h-full px-6 pt-4 mb-2 overflow-y-scroll scrollbar-hide">
+          <div className="flex flex-col w-full h-full">
+            {loading && (
+              <div className="flex flex-col w-full h-full justify-center items-center">
+                <CgSpinner className="w-12 h-12 text-gray-400 animate-spin" />
+              </div>
+            )}
+
+            {!loading && messages?.length === 0 && (
+              <div className="flex flex-col flex-1 items-center justify-center">
+                <IoChatbubbleOutline className="w-24 h-24 text-gray-300" />
+                <p className="text-2xl font-medium tracking-tight text-gray-300">
+                  대화를 시작하세요.
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+        <BottomBar user={user as User} chatId={id as string} />
       </div>
     </main>
   );
--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,7 +1,54 @@
 "use client";
 
-const BottomBar = () => {
-  return <div>BottomBar</div>;
+import { User } from "firebase/auth";
+import { db } from "@/firebase/firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { FormEvent, useState } from "react";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+
+interface IBottomBarProps {
+  user: User;
+  chatId: string;
+}
+
+const BottomBar = ({ user, chatId }: IBottomBarProps) => {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (input.trim().length === 0) return;
+
+    await addDoc(collection(db, `chats/${chatId}/messages`), {
+      text: input,
+      sender: user.email,
+      photoURL: user.photoURL,
+      timestamp: serverTimestamp(),
+    });
+
+    setInput("");
+  };
+
+  return (
+    <div className="sticky flex w-full bg-white top-full">
+      <form
+        onSubmit={sendMessage}
+        className="flex items-end w-full px-6 pb-4 space-x-2"
+      >
+        <input
+          value={input}
+          autoComplete="off"
+          className="w-full px-4 py-4 placeholder-gray-400 border border-gray-200 
+          rounded-lg focus:border-gray-400 focus:ring-0"
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="메시지를 입력하세요."
+        />
+        <button type="submit">
+          <IoPaperPlaneOutline className="mb-4 text-gray-600 w-7 h-7 hover:text-gray-900" />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default BottomBar;
