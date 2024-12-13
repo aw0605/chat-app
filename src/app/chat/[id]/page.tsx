63ca3de994eec8342ce099dc6bae5e05505cb719
aw0605 <101087917+aw0605@users.noreply.github.com>
2024-12-13 19:40:49 +0900

    TopBar 컴포넌트 구현

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -28,13 +28,17 @@ const ChatPage = () => {
 
   const [chat] = useDocumentData(doc(db, "chats", id as string));
 
+  const getOtherUser = (users: User[], currentUser: User) => {
+    return users?.filter((user) => user.email !== currentUser?.email)[0];
+  };
+
   return (
     <main className="grid w-full grid-cols-8">
       <div className="col-span-2">
         <SideBar selectedChatId={id as string} />
       </div>
       <div className="flex flex-col w-full col-span-6">
-        <TopBar />
+        <TopBar user={getOtherUser(chat?.usersData, user as User)} />
         <div className="flex w-full h-full px-6 pt-4 mb-2 overflow-y-scroll scrollbar-hide">
           <div className="flex flex-col w-full h-full">
             {loading && (
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,32 @@
 "use client";
 
-const TopBar = () => {
-  return <div>TopBar</div>;
+import { User } from "firebase/auth";
+import Image from "next/image";
+
+interface ITopbarProps {
+  user: User;
+}
+
+const TopBar = ({ user }: ITopbarProps) => {
+  return (
+    <div className="h-[70px] flex items-center w-full px-6 py-4 space-x-4 border-b-[1px]">
+      <Image
+        src={
+          user?.photoURL
+            ? user.photoURL
+            : "https://cdn.pixabay.com/photo/2021/07/25/08/03/account-6491185_640.png"
+        }
+        width={40}
+        height={40}
+        className="rounded-full object-cover"
+        alt={user?.displayName ?? user.email?.split("@")[0] ?? ""}
+      />
+
+      <span className="font-bold">
+        {user?.displayName ? user.displayName : user.email?.split("@")[0]}
+      </span>
+    </div>
+  );
 };
 
 export default TopBar;
